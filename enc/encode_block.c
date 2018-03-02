ff6559c45eb8822125383a8b18ba8d87b664f8bf
Yushin Cho <ycho@mozilla.com>
2018-03-02 10:59:51 -0800

    Fix comment where inter rdo start

--- enc/encode_block.c
+++ enc/encode_block.c
@@ -1908,10 +1908,10 @@ static int mode_decision_rdo(encoder_info_t *encoder_info,block_info_t *block_in
     }
   }
 
-  /* Evaluate inter mode */
   if ((size < 128 || encoder_info->params->encoder_speed == 0) &&
       !rectangular_flag && size <= MAX_TR_SIZE) { //Only evaluate intra or inter mode if the block is square
 
+    /* Evaluate inter mode */
     if (frame_type != I_FRAME){
       tb_param = 0;
       tmp_block_param.tb_param = 0;
